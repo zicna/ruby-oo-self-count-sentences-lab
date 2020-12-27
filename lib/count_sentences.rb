e3fcec029ce5d665f6e4c283e1bd97e89b3f882c
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2020-12-26 23:34:13 -0600

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,18 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".") ? true : false
   end
 
   def question?
-
+    self.end_with?("?") ? true : false
   end
 
   def exclamation?
-
+    self.end_with?("!") ? true : false
   end
 
   def count_sentences
-
+    self.split(/\.+\s|\?+\s|!+\s/).size
   end
 end
